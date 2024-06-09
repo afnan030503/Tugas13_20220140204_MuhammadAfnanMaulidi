dafc64db42354069fe38e2c07c7fe7cd86016648
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-06-09 08:27:23 +0700

    tugas selesai

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+import logo from './nodel.jpg';
 import './App.css';
 
 function App() {
+  const showAlert = () => {
+    alert('Semangat');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Muhammad Afnan Maulidi
         </p>
         <a
           className="App-link"
@@ -15,8 +19,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <p className="App-description">
+         Ini adalah pragraf baru supaya bisa lepas dari tugas
+        </p>
+        <button className="App-button" onClick={showAlert}>
+          Click Me
+        </button>
       </header>
     </div>
   );
